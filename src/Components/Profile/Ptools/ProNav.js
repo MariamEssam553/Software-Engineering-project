2c4178df549160c305331c2b8eb71262405f18d0
MariamEssam553 <83476787+MariamEssam553@users.noreply.github.com>
2022-03-28 23:09:59 +0200

    Add files via upload

---
+++ src/Components/Profile/Ptools/ProNav.js
@@ -0,0 +1,19 @@
+import './ProNav.css';
+
+const ProNav = () => (
+  <div>
+    <nav className="ProNv">
+      <div className="Plink">
+        <a href="/About">About </a>
+        <a href="/Photostream">Photostream</a>
+        <a href="/Albums">Albums</a>
+        <a href="/Faves">Faves</a>
+        <a href="/Profile/Galleries">Galleries</a>
+        <a href="/Groups">Groups</a>
+        <a href="/Stats">Stats</a>
+        <a href="/Profile/CameraRoll">Camera Roll</a>
+      </div>
+    </nav>
+  </div>
+);
+export default ProNav;
